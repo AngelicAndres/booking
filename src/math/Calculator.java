5156cbf89e9a6f6681140f5cf491109f100abd19
Alan Andres Garcia Pardo <alangarcia9517@gmail.com>
2024-06-11 20:43:57 -0600

    Resta agregada

--- src/math/Calculator.java
+++ src/math/Calculator.java
@@ -9,5 +9,7 @@ public class Calculator {
     public double sum(double a, double b) {
         return a + b;
     };
-
+    public double resta(double a, double b) {
+        return a - b;
+    };
 }
